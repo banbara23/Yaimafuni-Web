3d6b724b3eb841686c39c77646dbb862a786171b
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-19 11:58:45 +0900

    App.vueにdiv#containerを追加

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,42 @@
 <template>
   <div id="app">
     <navi />
-    <br>
-    <router-view></router-view>
+    <br />
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-  import firebase from 'firebase'
-  import firebaseconfig from '../firebase-config.json'
-  firebase.initializeApp(firebaseconfig);
+import firebase from 'firebase'
+import firebaseconfig from '../firebase-config.json'
+firebase.initializeApp(firebaseconfig);
 
-  import Navi from './components/Navi';
-  export default {
-    components: {
-      Navi
-    },
-    name: 'app'
-  }
+import Navi from './components/Navi';
+export default {
+  components: {
+    Navi
+  },
+  name: 'app'
+}
 </script>
 
 <style>
-  a {
-    color: #2c3e50;
-  }
-  
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    background-color: #F5F5F5;
-  }
-  
-  Navi {
-    text-align: left;
-  }
+a {
+  color: #2c3e50;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #F5F5F5;
+}
+
+Navi {
+  text-align: left;
+}
 </style>
