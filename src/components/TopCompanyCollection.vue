7c1e38300e877e3802e213b647847931f86c50b0
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-21 14:18:19 +0900

    Top会社別にクラスとスタイルのバインディングを適用

--- src/components/TopCompanyCollection.vue
+++ src/components/TopCompanyCollection.vue
@@ -1,39 +1,48 @@
 <template>
   <div>
     <div class="collection">
-      <a href="#" class="collection-item black-text">
-        安栄観光
-        <span v-if="company.anei.allNormalFlag" class="new badge blue" data-badge-caption="">{{company.anei.comment}}</span>
-        <span v-if="company.anei.cancelFlag"class="new badge red" data-badge-caption="">{{company.anei.comment}}</span>
-        <span v-if="company.anei.cationFlag"class="new badge orange" data-badge-caption="">{{company.anei.comment}}</span>
+      <a href="#"
+         class="collection-item black-text">
+            安栄観光
+            <span class="new badge"
+              v-bind:class="{blue:company.anei.allNormalFlag,
+              red:company.anei.cancelFlag,
+              orange:company.anei.cationFlag}" 
+              data-badge-caption="">{{company.anei.comment}}</span>
+          </a>
+  
+      <a href="#"
+        class="collection-item black-text">
+          八重山観光フェリー
+        <span class="new badge"
+              v-bind:class="{blue:company.ykf.allNormalFlag,
+              red:company.ykf.cancelFlag,
+              orange:company.ykf.cationFlag}" 
+              data-badge-caption="">{{company.ykf.comment}}</span>
       </a>
-
-      <a href="#" class="collection-item black-text">
-        八重山観光フェリー
-        <span v-if="company.ykf.allNormalFlag" class="new badge blue" data-badge-caption="">{{company.ykf.comment}}</span>
-        <span v-if="company.ykf.cancelFlag"class="new badge red" data-badge-caption="">{{company.ykf.comment}}</span>
-        <span v-if="company.ykf.cationFlag"class="new badge orange" data-badge-caption="">{{company.ykf.comment}}</span>
-      </a>
-      
-      <a href="#" class="collection-item black-text">
-        石垣ドリーム観光
-        <span v-if="company.dream.allNormalFlag" class="new badge blue" data-badge-caption="">{{company.dream.comment}}</span>
-        <span v-if="company.dream.cancelFlag"class="new badge red" data-badge-caption="">{{company.dream.comment}}</span>
-        <span v-if="company.dream.cationFlag"class="new badge orange" data-badge-caption="">{{company.dream.comment}}</span>
-        <span v-if="company.dream.suspendFlag"class="new badge orange" data-badge-caption="">{{company.dream.comment}}</span>
-      </a>
+  
+      <a href="#"
+         class="collection-item black-text">
+            石垣ドリーム観光
+        <span class="new badge"
+              v-bind:class="{blue:company.dream.allNormalFlag,
+              red:company.dream.cancelFlag,
+              orange:company.dream.cationFlag,
+              orange:company.dream.suspendFlag}" 
+              data-badge-caption="">{{company.dream.comment}}</span>
+          </a>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'row',
-    props: ['company'],
-    data() {
-      return {
-        title: '今日の天気'
-      }
+export default {
+  name: 'row',
+  props: ['company'],
+  data() {
+    return {
+      title: '今日の天気'
     }
   }
+}
 </script>
