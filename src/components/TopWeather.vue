4b3a55baaec4c99311becc0d46bdfc2877b9edc1
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-24 14:28:10 +0900

    リファクタ

--- src/components/TopWeather.vue
+++ src/components/TopWeather.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="weather">
-    
-    <div class="card hoverable">
+  <div class="main">
+    <div class="card">
       <div class="card-content black-text">
         <p>{{ weather.date }}</p>
         <p>{{ weather.weather }}、{{ weather.wind }}、{{ weather.wave }}</p>
@@ -13,14 +12,8 @@
 
 <script>
   export default {
-    name: 'row',
-    props: ['weather'],
-    data() {
-      return {
-        title: '4月17日(月)の天気',
-        msg: '曇後雨　波：1.5メートル後2メートル　風：南の風後やや強く'
-      }
-    }
+    name: 'main',
+    props: ['weather']
   }
 </script>
 
