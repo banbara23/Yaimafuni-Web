8c9242bb0cae392966973751f276ffef7baea763
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-04-26 14:43:54 +0900

    components配下を整理

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Top from '@/components/Top'
-import Weather from '@/components/Weather'
-import CompanyList from '@/components/CompanyList'
+import Top from '@/components/top/Top'
 import CompanyDetail from '@/components/CompanyDetail'
-import PortList from '@/components/PortList'
 import PortDetail from '@/components/PortDetail'
-import WeatherDetail from '@/components/WeatherDetail'
+import WeatherDetail from '@/components/weather/WeatherDetail'
 
 Vue.use(Router)
 
@@ -14,9 +11,7 @@ export default new Router({
   routes: [
   { path: '/', name: 'Top', component: Top },
   { path: '/weather', component: WeatherDetail },
-  { path: '/companyList', component: CompanyList },
   { path: '/companyDetail/:id', component: CompanyDetail },
-  { path: '/portList', component: PortList },
   { path: '/portDetail', component: PortDetail }
   ]
 })
