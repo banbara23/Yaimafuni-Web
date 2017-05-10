56014737e94546056ba32fcac6746e8cad9f9e59
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-05-10 13:43:48 +0900

    バッジの色をステータスに合わせて表示させた #2

--- src/components/top/Port.vue
+++ src/components/top/Port.vue
@@ -10,10 +10,10 @@
 				             class="collection-item black-text">
 					安栄観光
 					<span class="new badge"
-					      v-bind:class="classObject"
+					      v-bind:class="aneiColor"
 					      data-badge-caption="">
-						{{data.anei.status.text}}
-					</span>
+									{{data.anei.status.text}}
+								</span>
 				</router-link>
 	
 				<router-link to="/"
@@ -21,10 +21,10 @@
 				             class="collection-item black-text">
 					八重山観光フェリー
 					<span class="new badge"
-					      v-bind:class="classObject"
+					      v-bind:class="ykfColor"
 					      data-badge-caption="">
-						{{data.ykf.status.text}}
-					</span>
+									{{data.ykf.status.text}}
+								</span>
 				</router-link>
 	
 				<router-link to="/"
@@ -32,10 +32,10 @@
 				             class="collection-item black-text">
 					石垣ドリーム観光
 					<span class="new badge"
-					      v-bind:class="classObject"
+					      v-bind:class="dreamColor"
 					      data-badge-caption="">
-						{{data.dream.status.text}}
-					</span>
+									{{data.dream.status.text}}
+								</span>
 				</router-link>
 			</div>
 		</div>
@@ -45,11 +45,114 @@
 <script>
 export default {
 	name: 'port',
-	props: ['title','data'],
+	props: ['title', 'data'],
 	computed: {
 		classObject: function () {
-			return {
-				blue: true
+			if (this.data.anei.status.code == 'normal') {
+				return {
+					blue: true
+				}
+			}
+			else if (this.data.anei.status.code == 'cancel') {
+				return {
+					red: true
+				}
+			}
+			else if (this.data.anei.status.code == 'cation') {
+				return {
+					orange: true
+				}
+			}
+			else if (this.data.anei.status.code == 'suspend') {
+				return {
+					amber: true
+				}
+			}
+			else {
+				return {
+					orange: true
+				}
+			}
+		},
+		aneiColor: function () {
+			if (this.data.anei.status.code == 'normal') {
+				return {
+					blue: true
+				}
+			}
+			else if (this.data.anei.status.code == 'cancel') {
+				return {
+					red: true
+				}
+			}
+			else if (this.data.anei.status.code == 'cation') {
+				return {
+					orange: true
+				}
+			}
+			else if (this.data.anei.status.code == 'suspend') {
+				return {
+					amber: true
+				}
+			}
+			else {
+				return {
+					orange: true
+				}
+			}
+		},
+		ykfColor: function () {
+			if (this.data.ykf.status.code == 'normal') {
+				return {
+					blue: true
+				}
+			}
+			else if (this.data.ykf.status.code == 'cancel') {
+				return {
+					red: true
+				}
+			}
+			else if (this.data.ykf.status.code == 'cation') {
+				return {
+					orange: true
+				}
+			}
+			else if (this.data.ykf.status.code == 'suspend') {
+				return {
+					amber: true
+				}
+			}
+			else {
+				return {
+					orange: true
+				}
+			}
+		},
+		dreamColor: function () {
+			if (this.data.dream.status.code == 'normal') {
+				return {
+					blue: true
+				}
+			}
+			else if (this.data.dream.status.code == 'cancel') {
+				return {
+					red: true
+				}
+			}
+			else if (this.data.dream.status.code == 'cation') {
+				return {
+					orange: true
+				}
+			}
+			else if (this.data.dream.status.code == 'suspend') {
+				return {
+					orange: true
+				}
+			}
+			else {
+				return {
+					orange: true
+				}
 			}
 		}
 	}
