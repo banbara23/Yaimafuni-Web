a97339f66e0855ffc4d866ce7e5f184ed97fcf9e
ikemura <kazutaka.ikemura.024@ctc-g.co.jp>
2017-05-10 12:59:07 +0900

    consoleにエラーが大量に出ていた対応 #2

--- src/components/top/Port.vue
+++ src/components/top/Port.vue
@@ -2,30 +2,42 @@
 	<div id="port">
 		<div class="card collection with-header">
 			<div class="collection-header light-blue white-text">
-				<h5>{{data.anei.portName}}</h5>
+				<h5>{{title}}</h5>
 			</div>
-
-			<router-link to="/" v-if="data.anei" class="collection-item black-text">
-				安栄観光
-				<span class="new badge" v-bind:class="classObject" data-badge-caption="">
-					{{data.anei.status.text}}
-				</span>
-			</router-link>
+			<div v-if="data">
+				<router-link to="/"
+				             v-if="data.anei"
+				             class="collection-item black-text">
+					安栄観光
+					<span class="new badge"
+					      v-bind:class="classObject"
+					      data-badge-caption="">
+						{{data.anei.status.text}}
+					</span>
+				</router-link>
 	
-			<router-link to="/" v-if="data.ykf" class="collection-item black-text">
-				八重山観光フェリー
-				<span class="new badge" v-bind:class="classObject" data-badge-caption="">
-					{{data.ykf.status.text}}
-				</span>
-			</router-link>
-
-			<router-link to="/" v-if="data.dream" class="collection-item black-text">
-				石垣ドリーム観光
-				<span class="new badge" v-bind:class="classObject" data-badge-caption="">
-					{{data.dream.status.text}}
-				</span>
-			</router-link>
-			
+				<router-link to="/"
+				             v-if="data.ykf"
+				             class="collection-item black-text">
+					八重山観光フェリー
+					<span class="new badge"
+					      v-bind:class="classObject"
+					      data-badge-caption="">
+						{{data.ykf.status.text}}
+					</span>
+				</router-link>
+	
+				<router-link to="/"
+				             v-if="data.dream"
+				             class="collection-item black-text">
+					石垣ドリーム観光
+					<span class="new badge"
+					      v-bind:class="classObject"
+					      data-badge-caption="">
+						{{data.dream.status.text}}
+					</span>
+				</router-link>
+			</div>
 		</div>
 	</div>
 </template>
@@ -33,13 +45,13 @@
 <script>
 export default {
 	name: 'port',
-	props: ['data'],
+	props: ['title','data'],
 	computed: {
-		 classObject: function () {
-			 return {
-				 blue: true
-			 }
-		 }
+		classObject: function () {
+			return {
+				blue: true
+			}
+		}
 	}
 }
 </script>
--- src/components/top/Top.vue
+++ src/components/top/Top.vue
@@ -3,31 +3,38 @@
     <div class="row">
   
       <div class="col s12 m12 l6">
-        <port :data="port.taketomi" :title='竹富島航路' />
+        <port :data="port.taketomi"
+              :title="title.taketomi" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.kohama" :title='小浜島航路' />
+        <port :data="port.kohama"
+              :title="title.kohama" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.kuroshima" :title='黒島航路' />
+        <port :data="port.kuroshima"
+              :title="title.kuroshima" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.oohara" :title='大原航路' />
+        <port :data="port.uehara"
+              :title="title.uehara" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.uehara" :title='上原航路' />
+        <port :data="port.hatoma"
+              :title="title.hatoma" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.hatoma" :title='鳩間島航路' />
+        <port :data="port.oohara"
+              :title="title.oohara" />
       </div>
   
       <div class="col s12 m12 l6">
-        <port :data="port.hateruma" :title='波照間島航路' />
+        <port :data="port.hateruma"
+              :title="title.hateruma" />
       </div>
   
       <div class="col s12 m12 l6">
@@ -47,6 +54,19 @@ const db = firebase.database()
 
 export default {
   name: 'main',
+  data() {
+    return {
+      title: {
+        taketomi: '竹富島',
+        kohama: '小浜島',
+        kuroshima: '黒島',
+        oohara: '大原(西表島)',
+        uehara: '上原(西表島)',
+        hatoma: '鳩間島',
+        hateruma: '波照間島',
+      }
+    }
+  },
   firebase: {
     weatherToday: {
       source: db.ref('/weather/today'),
