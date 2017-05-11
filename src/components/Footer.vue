73349c0c32bce028faa2c39e824772a94558bb64
Kazutaka Ikemura <ikemura@KazutakanoMacBook-Pro.local>
2017-05-11 20:04:20 +0900

    コミット漏れ #7

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer id='footer' class="page-footer blue darken-2">
+    <div class="container">
+      <div class="row">
+        <!--<div class="col l6 s12">
+          <h5 class="white-text">Footer Content</h5>
+          <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        </div>-->
+
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+            <li>
+              <a class="grey-text text-lighten-3" href="http://aneikankou.co.jp/">安栄観光</a>
+            </li>
+            <li>
+              <a class="grey-text text-lighten-3" href="http://www.yaeyama.co.jp/">八重山観光フェリー</a>
+            </li>
+            <li>
+              <a class="grey-text text-lighten-3" href="http://ishigaki-dream.co.jp/">石垣島ドリーム観光</a>
+            </li>
+            <li>
+              <a class="grey-text text-lighten-3" href="https://play.google.com/store/apps/details?id=com.banbara.yaeyama.liner.checker">やいまふに - Android版</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2017 やいまふに
+        <!--<a class="grey-text text-lighten-4 right" href="#!">More Links</a>-->
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+  
+}
+</script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,15 @@
 <template>
-  <nav>
-    <div class="nav-wrapper blue darken-1">
-      <a href="/" class="brand-logo center">やいまふに</a>
-    </div>
-  </nav> 
+  <!--<nav>-->
+  <!--<div class="nav-wrapper blue darken-2">-->
+  <div id='navi' class="container">
+    <a href="/" class="brand-logo left white-text">やいまふに</a>
+  </div>
+  <!--</div>-->
+  <!--</nav> -->
 </template>
+<style  scoped>
+#navi {
+  overflow: auto;
+  font-size: xx-large;
+}
+</style>
