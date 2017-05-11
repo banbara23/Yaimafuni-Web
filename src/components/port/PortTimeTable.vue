d87ca8b10ec4b1f66ad869277de90295f7de21a7
Kazutaka Ikemura <ikemura@KazutakanoMacBook-Pro.local>
2017-05-11 23:43:30 +0900

    アイコンのwidthが<i>タグだと狂うのでローカルからimgで読み込む #4

--- src/components/port/PortTimeTable.vue
+++ src/components/port/PortTimeTable.vue
@@ -13,16 +13,16 @@
             <span v-if="row.left.memo">
               <br>{{row.left.memo}}</span>
           </td>
-          <td>
-            <i class="material-icons blue-text">{{getIcon(row.left.status.code)}}</i>
+          <td class="valign-wrapper">
+            <img v-if="row.left.status.code" :src="`../../static/${row.left.status.code}.svg`">
             {{row.left.status.text}}</td>
           <td>
             {{row.right.time}}
             <span v-if="row.right.memo">
               <br>{{row.right.memo}}</span>
           </td>
-          <td>
-            <i class="material-icons blue-text">{{getIcon(row.right.status.code)}}</i>
+          <td class="valign-wrapper">
+            <img v-if="row.right.status.code" :src="`../../static/${row.right.status.code}.svg`">
             {{row.right.status.text}}</td>
           </td>
         </tr>
@@ -37,54 +37,17 @@ const db = firebase.database()
 
 export default {
   name: 'main',
-  props: ['header', 'rows'],
-  methods: {
-    getStatus: (status) => {
-      // 仮表示
-      let codeText = '';
-      let statusText = '';
-      switch (status.code) {
-        case 'normal':
-          codeText = '○'
-          break;
-        case 'cancel':
-          codeText = '×'
-          break;
-        case 'cation':
-          codeText = '△'
-          break;
-        case 'suspend':
-          codeText = '△'
-          break;
-        case '':
-          codeText = ''
-          break;
-        default:
-          codeText = '△'
-          break;
-      }
-      return codeText + ' ' + status.text
-    },
-    getIcon: function (statusCode) {
-      switch (statusCode) {
-        case 'normal':
-          return 'ic_radio_button_unchecked'
-        case 'cancel':
-          return 'ic_clear'
-        case 'cation':
-          return 'ic_change_history'
-        case 'suspend':
-          return 'ic_change_history'
-        default:
-          return ''
-      }
-    }
-  }
+  props: ['header', 'rows']
 }
 </script>
 
 <style  scoped>
 table {
   background-color: #FFFFFF;
+}
+td img {
+  widows: 18px;
+  height: 18px;
+  margin-right: 4px;
 }
 </style>
