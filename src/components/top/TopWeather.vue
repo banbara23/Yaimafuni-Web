c3ee45dc56b70b963b4083ea14d79e18ce0eacc3
Kazutaka Ikemura <ikemura@KazutakanoMacBook-Pro.local>
2017-05-17 21:58:44 +0900

    文言修正

--- src/components/top/TopWeather.vue
+++ src/components/top/TopWeather.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <div class="card">
       <div class="card-content black-text" v-if="weather">
-        <span class="card-title">天気情報</span>
+        <span class="card-title">八重山の天気</span>
         <p>{{ weather.date }} {{ weather.weather }}</p>
         <p>風：{{ weather.wind }}</p>
         <p>波：{{ weather.wave }}</p>
