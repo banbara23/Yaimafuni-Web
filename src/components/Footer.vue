ab271d51957a61440eaf12886bac70cd9253a79d
池村和剛 <z1tg6774@h1408327.local>
2017-05-25 11:07:07 +0900

    フッター修正

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,12 +4,15 @@
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">About site</h5>
-          <p class="grey-text text-lighten-4">やいまふに（八重山方言で’八重山の船'）は、石垣島と八重山諸島を結ぶ船のリアルタイムな運行情報と、天気情報を簡単に見ることができます。</p>
+          <p class="grey-text text-lighten-4">やいまふに（八重山方言で’八重山の船'）は、石垣島と八重山諸島を結ぶ船の運行情報と天気情報をリアルタイムに見ることができます。</p>
         </div>
 
         <div class="col l4 offset-l2 s12">
           <h5 class="white-text">Links</h5>
           <ul>
+            <li>
+              <a class="grey-text text-lighten-3" href="https://play.google.com/store/apps/details?id=com.banbara.yaeyama.liner.checker">やいまふに - Android版</a>
+            </li>
             <li>
               <a class="grey-text text-lighten-3" href="http://aneikankou.co.jp/">安栄観光</a>
             </li>
@@ -18,9 +21,6 @@
             </li>
             <li>
               <a class="grey-text text-lighten-3" href="http://ishigaki-dream.co.jp/">石垣島ドリーム観光</a>
-            </li>
-            <li>
-              <a class="grey-text text-lighten-3" href="https://play.google.com/store/apps/details?id=com.banbara.yaeyama.liner.checker">やいまふに - Android版</a>
             </li>
           </ul>
         </div>
