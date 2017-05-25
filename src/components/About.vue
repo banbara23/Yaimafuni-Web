5a4179cc0346cf11d5ce2260c4bdf05de4a29176
池村和剛 <z1tg6774@h1408327.local>
2017-05-25 11:08:02 +0900

    About修正

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,20 +2,21 @@
   <div class="row">
     <div class="card">
       <div class="card-content">
-        <span class="card-title">About me</span>
-          <p>池村 和剛</p>
-          <p>西表島出身</p>
-          <p>石垣＆八重山を盛り上げるAndroidアプリ/Webサイトを制作しています。</p>
+        <span class="card-title">製作者</span>
+        <p>池村 和剛</p>
+        <p>西表島出身</p>
+        <p>石垣＆八重山を盛り上げるAndroidアプリ/Webサイトを制作しています。</p>
       </div>
     </div>
-
+  
     <div class="card">
       <div class="card-content">
         <span class="card-title">Connect</span>
-          <p>Gmail</p>
-          <p>Twitter</p>
-          <p>Facebook</p>
-          <p>Instaglam</p>
+        <p><a href="mailto:banbara.work@gmail.com">Mail</a></p>
+        <p><a href="https://twitter.com/ikemura_jp">Twitter @ikemura_jp</a></p>
+        <!--<p>Facebook</p>-->
+        <!--<p>Instaglam</p>-->
+        <!--https://blog.hubspot.jp/the-ultimate-cheat-sheet-for-creating-social-media-buttons-->
       </div>
     </div>
   </div>
@@ -28,9 +29,8 @@ export default {
 </script>
 
 <style scoped>
-  p {
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
-
+p {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
