de65f92006c77ba8ffb91522a2d551ff121a4e27
池村和剛 <z1tg6774@h1408327.local>
2017-06-01 15:48:16 +0900

    天気詳細のデザインを良さげに修正

--- src/components/weather/WeatherDetail.vue
+++ src/components/weather/WeatherDetail.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="app">
     <div class="row">
-      <div class="card">
-        <div class="col s12 m12 l6">
-          <div class="card-panel">
-            <span class="card-title">今日の天気</span>
-            <div v-if="weather.today">
-              <p>{{weather.today.date}} {{weather.today.weather}}</p>
-              <p>気温</p>
-              <p>　最高{{weather.today.temperature.hight}}</p>
-              <p>　最低{{weather.today.temperature.low}}</p>
-              <p>風：{{weather.today.wind}}</p>
-              <p>波：{{weather.today.wave}}</p>
-            </div>
+      <div class="col s12 m12 l6">
+        <div class="card">
+          <div id="card-header"
+               class="blue darken-3 white-text">
+            <h4>今日の天気</h4> {{weather.today.date}}
           </div>
+          <div class="card-panel flow-text">
+                <div v-if="weather.today">
+                  <p>天気：{{weather.today.weather}}</p>
+                  <p>気温
+                    <br />　最高{{weather.today.temperature.hight}}
+                    <br />　最低{{weather.today.temperature.low}}</p>
+                  <p>風：{{weather.today.wind}}</p>
+                  <p>波：{{weather.today.wave}}</p>
+                </div>
+              </div>
         </div>
       </div>
   
-      <div class="card">
-        <div class="col s12 m12 l6">
-          <div class="card-panel"
+      <div class="col s12 m12 l6">
+        <div class="card">
+          <div id="card-header"
+               class="blue darken-3 white-text">
+            <h4>明日の天気</h4>
+            <span v-if="weather.tomorrow">{{weather.tomorrow.date}}</span>
+          </div>
+          <div class="card-panel flow-text"
                v-if="weather.tomorrow">
-            <span class="card-title">明日の天気</span>
-            <div v-if="weather.today">
-              <p>{{weather.tomorrow.date}} {{weather.tomorrow.weather}}</p>
-              <p>気温</p>
-              <p>　最高{{weather.tomorrow.temperature.hight}}</p>
-              <p>　最低{{weather.tomorrow.temperature.low}}</p>
-              <p>風：{{weather.tomorrow.wind}}</p>
-              <p>波：{{weather.tomorrow.wave}}</p>
-            </div>
+            <p>天気：{{weather.tomorrow.weather}}</p>
+            <p>気温
+              <br />　最高{{weather.tomorrow.temperature.hight}}
+              <br />　最低{{weather.tomorrow.temperature.low}}</p>
+            <p>風：{{weather.tomorrow.wind}}</p>
+            <p>波：{{weather.tomorrow.wave}}</p>
           </div>
         </div>
+      </div>
+      <div class="col s12 m12 l6">
+        <a class="waves-effect waves-teal btn-large orange"
+           href="https://weather.yahoo.co.jp/weather/47/9410.html">天気を詳しく見る</a>
       </div>
     </div>
   </div>
@@ -43,10 +52,37 @@ const db = firebase.database()
 
 export default {
   name: 'app',
+  data() {
+    return {
+      weather: null
+    }
+  },
   firebase: {
     weather: {
       source: db.ref('/weather'),
       asObject: true
+    }
+  },
+  created() {
+    this.weather = {
+      today: {
+        date: '/',
+        temperature: {
+          hight: '',
+          low:''
+        },
+        wind: '',
+        wave: ''
+      },
+      tomorrow: {
+         date: '/',
+        temperature: {
+          hight: '',
+          low:''
+        },
+        wind: '',
+        wave: ''
+      }
     }
   }
 }
