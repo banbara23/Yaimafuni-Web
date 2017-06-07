affa61cf28671acc4a8949e9f6b8f124e8bea6c1
池村和剛 <z1tg6774@h1408327.local>
2017-06-07 14:05:08 +0900

    modify profile in about

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,20 +3,21 @@
     <div class="card">
       <div class="card-content">
         <span class="card-title">製作者</span>
+        <img src="../../static/about_pic.jpg" alt="ikemura photo" />
         <p>池村 和剛</p>
-        <p>西表島出身</p>
-        <p>石垣＆八重山を盛り上げるAndroidアプリ/Webサイトを制作しています。</p>
+        <p>西表島の浦内出身、いまは内地でソフトウェアエンジニア</p>
+        <p>石垣＆八重山を盛り上げるAndroidアプリ & Webサイトを制作しています。</p>
+        <br />
+        <a href="https://play.google.com/store/apps/details?id=com.banbara.yaeyama.liner.checker&hl=ja">やいまふに Android</a>
       </div>
     </div>
   
     <div class="card">
       <div class="card-content">
-        <span class="card-title">Connect</span>
-        <p><a href="mailto:banbara.work@gmail.com">Mail</a></p>
-        <p><a href="https://twitter.com/ikemura_jp">Twitter @ikemura_jp</a></p>
-        <!--<p>Facebook</p>-->
-        <!--<p>Instaglam</p>-->
-        <!--https://blog.hubspot.jp/the-ultimate-cheat-sheet-for-creating-social-media-buttons-->
+        <span class="card-title">Contact</span>
+        <p>メールは<a href="mailto:banbara.work@gmail.com">こちら</a></p>
+        <p>Twitter：<a href="https://twitter.com/ikemura_jp">@ikemura_jp</a></p>
+        <p>Facebook：<a href="https://www.facebook.com/banbaraniisan">池村 和剛</a></p>
       </div>
     </div>
   </div>
