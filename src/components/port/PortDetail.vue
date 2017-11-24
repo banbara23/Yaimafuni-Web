674f5eaf5ec4217f0482aea84b460463e71fbb09
ikemura <8417910+banbara23@users.noreply.github.com>
2017-11-24 21:16:52 +0900

    change timeTable path

--- src/components/port/PortDetail.vue
+++ src/components/port/PortDetail.vue
@@ -8,20 +8,20 @@
   
     <div class="row"
          v-if="timeTable">
-      <time-table :header='detail.timeTable.header'
-                  :rows='detail.timeTable.row' />
+      <time-table :header='timeTable.header'
+                  :rows='timeTable.row' />
     </div>
   </div>
 </template>
 
 <script>
-import TimeTable from './PortTimeTable';
-import Card from './Card';
-import firebase from 'firebase'
-const db = firebase.database()
+import TimeTable from "./PortTimeTable";
+import Card from "./Card";
+import firebase from "firebase";
+const db = firebase.database();
 
 export default {
-  name: 'portDetail',
+  name: "portDetail",
   components: {
     TimeTable,
     Card
@@ -31,28 +31,38 @@ export default {
       params: this.$route.params,
       detail: null,
       timeTable: null
-    }
+    };
   },
   firebase() {
     return {
       detail: {
-        source: db.ref(this.$route.params.company)
+        source: db
+          .ref(this.$route.params.company)
           .child(this.$route.params.port),
         asObject: true,
-        readyCallback: function () {
+        readyCallback: function() {
+          this.$Progress.finish();
+        }
+      },
+      timeTable: {
+        source: db
+          .ref(`${this.$route.params.company}_timeTable`)
+          .child(this.$route.params.port),
+        asObject: true,
+        readyCallback: function() {
           this.$Progress.finish();
         }
       }
-    }
+    };
   },
   created() {
     this.$Progress.start();
     this.detail = {
-      portName: '...',
-      comment: '',
+      portName: "...",
+      comment: "",
       status: {
-        code: '',
-        text: ''
+        code: "",
+        text: ""
       },
       timeTable: {
         header: {
@@ -61,65 +71,66 @@ export default {
         },
         rows: []
       }
-    }
+    };
 
-    if (this.$route.params.company == 'dream') {
+    if (this.$route.params.company == "dream") {
       this.timeTable = null;
-    }
-    else {
+    } else {
       this.timeTable = {
         header: {
-          left: ' ',
-          right: ' '
+          left: " ",
+          right: " "
         }
-      }
-      this.timeTable.row = [{
-        left: {
-          time: '',
-          memo: '',
-          status: {}
-        },
-        right: {
-          time: '',
-          memo: '',
-          status: {}
+      };
+      this.timeTable.row = [
+        {
+          left: {
+            time: "",
+            memo: "",
+            status: {}
+          },
+          right: {
+            time: "",
+            memo: "",
+            status: {}
+          }
         }
-      }]
+      ];
     }
   },
   computed: {
-    portName: function () {
+    portName: function() {
       // パラメータから港名を設定
       switch (this.$route.params.port) {
-        case 'taketomi':
-          return '竹富島航路'
-        case 'kohama':
-          return '小浜島航路'
-        case 'kuroshima':
-          return '黒島航路'
-        case 'uehara':
-          return '上原(西表島)航路'
-        case 'oohara':
-          return '大原(西表島)航路'
-        case 'hatoma':
-          return '鳩間島航路'
-        case 'hateruma':
-          return '波照間島航路'
+        case "taketomi":
+          return "竹富島航路";
+        case "kohama":
+          return "小浜島航路";
+        case "kuroshima":
+          return "黒島航路";
+        case "uehara":
+          return "上原(西表島)航路";
+        case "oohara":
+          return "大原(西表島)航路";
+        case "hatoma":
+          return "鳩間島航路";
+        case "hateruma":
+          return "波照間島航路";
         default:
-          return ''
+          return "";
       }
     },
-    companyName: function () {
+    companyName: function() {
       // パラメータから会社名を設定
       switch (this.$route.params.company) {
-        case 'anei':
-          return '安栄観光'
-        case 'ykf':
-          return '八重山観光フェリー'
-        case 'dream':
-          return '石垣ドリーム観光'
+        case "anei":
+          return "安栄観光";
+        case "ykf":
+          return "八重山観光フェリー";
+        case "dream":
+          return "石垣ドリーム観光";
       }
     }
   }
-}
+};
 </script>
